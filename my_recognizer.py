abaa5e1148c0431c1cebc90784455dfd635770ed
Ivar Prudnikov <ivar.prudnikov@gmail.com>
2017-11-04 09:02:20 +0000

    implement recognizer (part 3)

--- my_recognizer.py
+++ my_recognizer.py
@@ -1,4 +1,7 @@
 import warnings
+from operator import itemgetter
+import numpy as np
+
 from asl_data import SinglesData
 
 
@@ -10,7 +13,7 @@ def recognize(models: dict, test_set: SinglesData):
    :param test_set: SinglesData object
    :return: (list, list)  as probabilities, guesses
        both lists are ordered by the test set word_id
-       probabilities is a list of dictionaries where each key a word and value is Log Liklihood
+       probabilities is a list of dictionaries where each key is a word and value is Log Likelihood
            [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
             {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
             ]
@@ -20,6 +23,22 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+
+    for i in list(test_set.get_all_sequences().keys()):
+        scores = list()
+        X, lengths = test_set.get_item_Xlengths(i)
+        for word, gaussian in models.items():
+            log_l = -np.inf
+            try:
+                log_l = gaussian.score(X, lengths)
+            except:
+                pass
+
+            scores.append((word, log_l))
+
+        scores = sorted(scores, key=itemgetter(1), reverse=True)  # sort by log value; descending
+
+        probabilities.append(dict(scores))  # add sorted probabilities as dict
+        guesses.append(scores[0][0])  # select first word
+
+    return probabilities, guesses
